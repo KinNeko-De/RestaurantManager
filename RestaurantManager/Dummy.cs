571fad5ce0b5f846617bd6109409460648b81d47
Nils Diekmann <nils@diekmann-germany.de>
2022-07-09 19:38:15 +0200

    - added tests

---
+++ RestaurantManager/Dummy.cs
@@ -0,0 +1,14 @@
+﻿namespace RestaurantManager;
+
+public class Dummy
+{
+    public Dummy()
+    {
+
+    }
+
+    public bool TestMe(bool one, bool two)
+    {
+        return one == two;
+    }
+}
